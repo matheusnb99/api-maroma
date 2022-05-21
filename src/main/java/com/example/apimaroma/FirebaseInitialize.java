8a2f60dfa7948a19ac812119a75b6c63830b7668
matheusnb99 <matheusintelectual123@hotmail.com>
2022-05-21 12:35:57 +0200

    changed way springboot reads firebase file

--- src/main/java/com/example/apimaroma/FirebaseInitialize.java
+++ src/main/java/com/example/apimaroma/FirebaseInitialize.java
@@ -3,23 +3,24 @@ package com.example.apimaroma;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
-import java.io.FileInputStream;
+import java.io.InputStream;
 
 @Service
-@Primary
 public class FirebaseInitialize {
+    @Value("${app.firebase-configuration-file}")
+    private String firebaseConfigPath;
+
     @PostConstruct
     public void initialize(){
         try {
             System.out.println("-----------------------path");
 
-            FileInputStream serviceAccount =
-                    new FileInputStream("./serviceAccountKeyMaroma.json");
+            InputStream serviceAccount = new ClassPathResource(firebaseConfigPath).getInputStream();
 
             FirebaseOptions options = new FirebaseOptions.Builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
