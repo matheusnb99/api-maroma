6e1902767fb7736ffbb1e6f603fc92df2627f30c
matheusnb99 <matheusintelectual123@hotmail.com>
2022-05-29 18:27:08 +0200

    added search to categories route

--- src/main/java/com/example/apimaroma/categories/CategoryController.java
+++ src/main/java/com/example/apimaroma/categories/CategoryController.java
@@ -1,13 +1,12 @@
 package com.example.apimaroma.categories;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeoutException;
 
 @RestController
 @RequestMapping(path="api/v1/categories")
@@ -24,7 +23,12 @@ public class CategoryController {
         return categoryService.getCategory(id);
     }
     @GetMapping
-    public List<CategoryBean> getCategories() throws ExecutionException, InterruptedException {
+    public List<CategoryBean> getCategories(@RequestParam("search") Optional<String> search) throws ExecutionException, InterruptedException, TimeoutException {
+
+        if(search.isPresent()){
+            return categoryService.searchCategoryByTitle(search.get());
+        }
+
         return categoryService.getAllCategories();
     }
 
