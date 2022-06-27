dd63f8b43b1c88eb22431b9d618bdf90583b9e3b
matheusnb99 <matheusintelectual123@hotmail.com>
2022-06-27 16:32:08 +0200

    added img route

---
+++ src/main/java/com/example/apimaroma/orders/OrderControllerV2.java
@@ -0,0 +1,2 @@
+package com.example.apimaroma.orders;public class OrderControllerV2 {
+}
---
+++ src/main/java/com/example/apimaroma/products/ImageController.java
@@ -0,0 +1,39 @@
+package com.example.apimaroma.products;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.concurrent.ExecutionException;
+
+@RestController
+@RequestMapping(path = "api/v1/image")
+public class ImageController {
+    ProductModel productModel = new ProductModel();
+
+    @GetMapping(path="{id}", produces = MediaType.IMAGE_PNG_VALUE)
+    public byte[] getImage(@PathVariable String id) throws ExecutionException, InterruptedException, IOException {
+        String image = productModel.findById(id).get().getImages().get(0);
+
+        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+        HttpGet request = new HttpGet(image);
+        HttpResponse response = httpClient.execute(request);
+
+        if(response.getStatusLine().getStatusCode() == 200) {
+            System.out.println("response:: " + response.getEntity());
+            InputStream content = response.getEntity().getContent();
+            return content.readAllBytes();
+        }
+
+    return null;
+    }
+}
